8031f7be00562a60feddc04ab04cca526ebf8a4a
bekzatsky <mn.bekzat@gmail.com>
2019-02-26 17:51:26 +0600

    fixes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Info from './components/Info';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const API_KEY = '8241a399526c5722e7c637562f35c4b9';
+
+class App extends React.Component {
+
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        sunrise: undefined,
+        sunset: undefined,
+        error: undefined
+    };
+
+    getTime = (unix) => {
+        var date = new Date(unix * 1000);
+        var hour = date.getHours();
+        var minutes = "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+        return hour + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)
+    };
+
+    gettingWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+
+
+        if (city) {
+            const api_url = await
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await api_url.json();
+
+            var sunset = data.sys.sunset;
+            var sunset_date = this.getTime(sunset);
+            var sunrise = data.sys.sunrise;
+            var sunrise_date = this.getTime(sunrise);
+
+
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                sunrise: sunrise_date,
+                sunset: sunset_date,
+                error: undefined
+
+            });
+        } else {
+            this.setState({
+                temp: undefined,
+                city: undefined,
+                country: undefined,
+                sunrise: undefined,
+                sunset: undefined,
+                error: 'Enter the name of the city'
+            });
+        }
+    };
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-5 info">
+                                <Info/>
+                            </div>
+                            <div className="col-sm-7 form">
+                                <Form weatherMethod={this.gettingWeather}/>
+                                <Weather
+                                    temp={this.state.temp}
+                                    city={this.state.city}
+                                    country={this.state.country}
+                                    sunrise={this.state.sunrise}
+                                    sunset={this.state.sunset}
+                                    error={this.state.error}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Weather = props => (
+    <div className="infoWeath">
+        {props.city &&
+        <div>
+            <p>Location: {props.city}, {props.country}</p>
+            <p>Temperature: {props.temp}°C</p>
+            <p>Sunrise: {props.sunrise}</p>
+            <p>Sunset: {props.sunset}</p>
+        </div>}
+        <p className="error">{props.error}</p>
+    </div>
+);
+
+export default Weather;
